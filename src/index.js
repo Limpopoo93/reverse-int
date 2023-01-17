515adc2e71d425453602a29faffe8ff470bf0d79
sigchi-web <sigchi.dev@gmail.com>
2023-01-17 15:55:00 +0300

    init: reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  let result = '';
+    for (let i = 0; i < n.toString().length; i++) {
+        result = n.toString()[i] + result;
+    }
+    return parseInt(result);
 }
